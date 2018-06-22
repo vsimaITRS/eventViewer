1debf73dfe862aab99c1a667786bacd955165802
Vlad Sima <vsima@ldn.itrs>
2018-06-22 11:06:05 -0400

    Added format sunburst feature and made a call to return its json

--- src/main/java/com/eventViewer/api/controller/EventController.java
+++ src/main/java/com/eventViewer/api/controller/EventController.java
@@ -1,7 +1,14 @@
 package com.eventViewer.api.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,6 +17,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eventViewer.api.model.Event;
@@ -23,22 +31,56 @@ public class EventController {
     @Autowired
     private EventRepository eventRepository;
 
-    @RequestMapping(value = "/",
+    @RequestMapping(value = "",
     		produces = { MediaType.APPLICATION_JSON_VALUE },
     		method = RequestMethod.GET)
-    public ResponseEntity<List<Event>> getAllEvents() {
+    public ResponseEntity<List<Event>> getAllEvents(@RequestParam("start") Optional<Long> start,
+			  @RequestParam("end")   Optional<Long> end) {
     	
-    	List<Event> events = eventRepository.findAll();
+    	List<Event> events; 
+    	
+    	if(start != null && end != null)
+    		events = eventRepository.findEventsBetweenOptionalTimes(start, end);
+    	else if(start != null && end == null)
+    		events = eventRepository.findEventsBetweenOptionalTimes(start, null);
+    	else if(start == null && end != null)
+    		events = eventRepository.findEventsBetweenOptionalTimes(null, end);
+    	else
+    		events = eventRepository.findAll();
+    	    	
     	if(events.isEmpty())
     		return new ResponseEntity(HttpStatus.NO_CONTENT);
         return new ResponseEntity<List<Event>>(events, HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/zoomableSunburstJSON",
+    		produces = { MediaType.APPLICATION_JSON_VALUE },
+    		method = RequestMethod.GET)
+    public ResponseEntity<String> getZoomableSunburstJSON(@RequestParam("start") Optional<Long> start,
+    													  @RequestParam("end")   Optional<Long> end) throws JSONException {
+    	
+    	String formattedJSON;
+    	
+    	if(false)
+    		return new ResponseEntity(HttpStatus.NO_CONTENT);
+    	else {
+        	if(start != null && end != null)
+        		formattedJSON = formatZoomableSunburst(start, end);
+        	else if(start != null && end == null)
+        		formattedJSON = formatZoomableSunburst(start, null);
+        	else if(start == null && end != null)
+        		formattedJSON = formatZoomableSunburst(null, end);
+        	else
+        		formattedJSON = formatZoomableSunburst(null, null);
+    		return new ResponseEntity<String>(formattedJSON, HttpStatus.OK);
+    	}
     }
     
     //GET DISTINCT GATEWAYS FROM ALL EVENTS
     @RequestMapping(value = "/gateways", method = RequestMethod.GET)
     public ResponseEntity<List<String>> getDistinctGateways() {
     	
-    	List<String> gateways = eventRepository.findDistinctGateways();
+    	List<String> gateways = eventRepository.findDistinctGateways(null, null);
     	if(gateways.isEmpty())
     		return new ResponseEntity(HttpStatus.NO_CONTENT);    	
     	return new ResponseEntity<List<String>>(gateways, HttpStatus.OK);
@@ -48,7 +90,7 @@ public class EventController {
     @RequestMapping(value = "/probes", method = RequestMethod.GET)
     public ResponseEntity<List<String>> getDistinctProbes() {
     	
-    	List<String> probes = eventRepository.findDistinctProbes();
+    	List<String> probes = eventRepository.findDistinctProbes(null, null);
     	if(probes.isEmpty())
     		return new ResponseEntity(HttpStatus.NO_CONTENT);    	
     	return new ResponseEntity<List<String>>(probes, HttpStatus.OK);
@@ -59,7 +101,7 @@ public class EventController {
     @RequestMapping(value = "/probes/{probe}/samplers", method = RequestMethod.GET)
     public ResponseEntity<List<String>> getDistinctSamplersByProbeName(@PathVariable("probe") String probe) {
     	
-    	List<String> samplers = eventRepository.findDistinctSamplersByProbe(probe);
+    	List<String> samplers = eventRepository.findDistinctSamplersByProbe(probe, null, null);
     	if(samplers.isEmpty())
     		return new ResponseEntity(HttpStatus.NO_CONTENT);
     	
@@ -67,10 +109,110 @@ public class EventController {
     }
     
     //GET EVENT COUNT BASED ON SPECIFIC PROBE, SAMPLER, AND SEVERITY
-    @RequestMapping(value = "/probes/{probe}/samplers/{sampler}/severity/{severity}", method = RequestMethod.GET)
-    public ResponseEntity<?> getEventCountByProbeSamplerSeverity(@PathVariable("probe") String probe, @PathVariable("sampler") String sampler, @PathVariable("severity") String severity) {
+    @RequestMapping(value = "/gateways/{gateway}/probes/{probe}/samplers/{sampler}/severity/{severity}", method = RequestMethod.GET)
+    public ResponseEntity<?> getEventCountByProbeSamplerSeverity(@PathVariable("gateway") String gateway, @PathVariable("probe") String probe, @PathVariable("sampler") String sampler, @PathVariable("severity") String severity) {
     	
-    	int count = eventRepository.findEventCountByProbeSamplerSeverity(probe, sampler, severity);
+    	int count = eventRepository.findEventCountBySeverity(gateway, probe, sampler, severity, null, null);
     	return new ResponseEntity(count, HttpStatus.OK);
     }
+    
+    
+    public String formatZoomableSunburst(Optional<Long> start, Optional<Long> end) throws JSONException {
+    
+    	JSONObject flare = new JSONObject();
+			flare.put("name", "flare");
+			
+			JSONArray flareChildren = new JSONArray();
+			
+			List<String> gateways = eventRepository.findDistinctGateways(start, end);
+			
+			JSONArray gatewayChildren = new JSONArray();
+			
+			for(String gatewayName : gateways)	{
+				
+				JSONObject gateway = new JSONObject();
+				gateway.put("name", gatewayName);
+				gateway.put("children", gatewayChildren);
+				
+				flareChildren.put(gateway);
+				
+				List<String> netProbes = eventRepository.findDistinctProbesByGateway(gatewayName, start, end);
+				
+				JSONArray netProbeChildren = new JSONArray();
+				
+				for(String netProbeName : netProbes) {
+					
+					JSONObject netProbe = new JSONObject();
+					netProbe.put("name", netProbeName);
+					netProbe.put("children", netProbeChildren);
+					
+					gatewayChildren.put(netProbe);
+					
+					List<String> samplers = eventRepository.findDistinctSamplersByProbe(netProbeName, start, end);
+					
+					for(String samplerName : samplers) {
+						
+						JSONArray samplerChildren = new JSONArray();
+						
+						JSONObject sampler = new JSONObject();
+						sampler.put("name", samplerName);
+						sampler.put("children", samplerChildren);
+						
+						netProbeChildren.put(sampler);
+						
+						JSONObject undefined = new JSONObject();
+						JSONObject OK = new JSONObject();
+						JSONObject warning = new JSONObject();
+						JSONObject critical = new JSONObject();
+						
+						undefined.put("name", "undefined");
+						undefined.put("size", eventRepository.findEventCountBySeverity(gatewayName, netProbeName, samplerName, "-1", start, end));
+						
+						OK.put("name", "OK");
+						OK.put("size", eventRepository.findEventCountBySeverity(gatewayName, netProbeName, samplerName, "0", start, end));
+						
+						warning.put("name", "warning");
+						warning.put("size", eventRepository.findEventCountBySeverity(gatewayName, netProbeName, samplerName, "1", start, end));
+						
+						critical.put("name", "critical");
+						critical.put("size", eventRepository.findEventCountBySeverity(gatewayName, netProbeName, samplerName, "2", start, end));
+						
+						samplerChildren.put(undefined);
+						samplerChildren.put(OK);
+						samplerChildren.put(warning);
+						samplerChildren.put(critical);
+											
+					}
+				}
+			}
+			
+			flare.put("children", flareChildren);
+			
+			return flare.toString();
+    }
+    
+    
+//    public Set findDistinctGateways(List<Event> events) {
+//    	
+//    	List<String> gatewayList = new ArrayList();
+//    	
+//    	for(Event e : events) 
+//    		gatewayList.add(e.getGateway());
+//    	
+//    	return new HashSet(gatewayList);
+//    }
+//    
+//    public Set findDistinctNetProbesByGateway(List<Event> events, String gateway) {
+//
+//    	List<String> netProbeList = new ArrayList();
+//    	
+//    	for(Event e : events) 
+//    		if(e.getGateway().equals(gateway))
+//    			netProbeList.add(e.getGateway());
+//    	
+//    	return new HashSet(netProbeList);
+//    }
+    
+
+    
 }
--- src/main/java/com/eventViewer/data/EventRepository.java
+++ src/main/java/com/eventViewer/data/EventRepository.java
@@ -3,6 +3,7 @@ package com.eventViewer.data;
 import com.eventViewer.api.model.Event;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,18 +14,24 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
-
-	@Query("SELECT DISTINCT gateway FROM Event e")
-	public List findDistinctGateways();
 	
-	@Query("SELECT DISTINCT probe FROM Event e")
-	public List findDistinctProbes();
-	
-	@Query("SELECT DISTINCT sampler FROM Event e WHERE probe = :probe")
-	public List findDistinctSamplersByProbe(@Param("probe") String probe);
+	@Query("FROM Event e WHERE (:start is NULL or e.timestamp >= :start) AND (:end is NULL or e.timestamp <= :end)")
+	public List findEventsBetweenOptionalTimes(@Param("start") Optional<Long> start, @Param("end") Optional<Long> end);
 
-	@Query("SELECT COUNT(severity) FROM Event e WHERE sampler = :sampler AND probe = :probe AND severity = :severity")
-	public int findEventCountByProbeSamplerSeverity(@Param("probe") String probe, @Param("sampler") String sampler, @Param("severity") String severity);
+	@Query("SELECT DISTINCT gateway FROM Event e WHERE (:start is NULL or e.timestamp >= :start) AND (:end is NULL or e.timestamp <= :end)")
+	public List findDistinctGateways(@Param("start") Optional<Long> start, @Param("end") Optional<Long> end);
+	
+	@Query("SELECT DISTINCT probe FROM Event e WHERE (:start is NULL or e.timestamp >= :start) AND (:end is NULL or e.timestamp <= :end)")
+	public List findDistinctProbes(@Param("start") Optional<Long> start, @Param("end") Optional<Long> end);
+	
+	@Query("SELECT DISTINCT probe FROM Event e WHERE gateway = :gateway AND (:start is NULL or e.timestamp >= :start) AND (:end is NULL or e.timestamp <= :end)")
+	public List findDistinctProbesByGateway(@Param("gateway") String gateway, @Param("start") Optional<Long> start, @Param("end") Optional<Long> end);
+	
+	@Query("SELECT DISTINCT sampler FROM Event e WHERE probe = :probe AND (:start is NULL or e.timestamp >= :start) AND (:end is NULL or e.timestamp <= :end)")
+	public List findDistinctSamplersByProbe(@Param("probe") String probe, @Param("start") Optional<Long> start, @Param("end") Optional<Long> end);
+
+	@Query("SELECT COUNT(severity) FROM Event e WHERE gateway = :gateway AND sampler = :sampler AND probe = :probe AND severity = :severity AND (:start is NULL or e.timestamp >= :start) AND (:end is NULL or e.timestamp <= :end)")
+	public int findEventCountBySeverity(@Param("gateway") String gateway, @Param("probe") String probe, @Param("sampler") String sampler, @Param("severity") String severity, @Param("start") Optional<Long> start, @Param("end") Optional<Long> end);
 	
 }
 
